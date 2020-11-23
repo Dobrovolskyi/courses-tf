71c5b56940345fe81c6686756f88e6addffd7ec8
dobrovolskyi <vdobrovolskyi@gmail.com>
2020-11-23 22:24:03 +0200

    GCP part added

--- instances.tf
+++ instances.tf
@@ -1,3 +1,7 @@
+#=========================================================================
+#                         Amazon Web Services
+#=========================================================================
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -28,5 +32,46 @@ resource "aws_instance" "web_iter" {
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld-${each.key}"
+  }
+}
+
+
+#=========================================================================
+#                         Google Cloud Platform
+#=========================================================================
+
+resource "google_compute_instance_template" "tf-instance" {
+  name        = "appserver-1"
+  description = "first app server instances."
+
+  tags = ["app", "DevOps-course"]
+
+  labels = {
+    environment = "dev"
+    service     = "app-server"
+  }
+
+  instance_description = "minimal instance"
+  machine_type         = "e2-micro"
+  can_ip_forward       = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  // Create a new boot disk from an image
+  disk {
+    source_image = "ubuntu-minimal-2004-focal-v20201028"
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
   }
 }
