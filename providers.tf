954b2561c041b6e202cbd782e0b16dc28ca6581c
dobrovolskyi <vdobrovolskyi@gmail.com>
2020-11-23 23:02:23 +0200

    google provider block extended

--- providers.tf
+++ providers.tf
@@ -8,5 +8,9 @@ provider "aws" {
 }
 
 provider "google" {
-  version = "3.48.0"
+  version     = "3.48.0"
+  credentials = "${GOOGLE_APPLICATION_CREDENTIALS}"
+  project     = "My First Project" # "{{YOUR GCP PROJECT}}"
+  region      = "europe-west1"
+  zone        = "europe-west1-b"
 }
