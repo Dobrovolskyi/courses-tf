fa07378ebba8892f615e25431c262580cffa02ac
dobrovolskyi <vdobrovolskyi@gmail.com>
2020-11-24 22:28:50 +0200

    gcp creds via var.

--- providers.tf
+++ providers.tf
@@ -9,7 +9,7 @@ provider "aws" {
 
 provider "google" {
   version     = "3.48.0"
-  credentials = "${GOOGLE_APPLICATION_CREDENTIALS}"
+  credentials = var.GOOGLE_APPLICATION_CREDENTIALS
   project     = "My First Project" # "{{YOUR GCP PROJECT}}"
   region      = "europe-west1"
   zone        = "europe-west1-b"
