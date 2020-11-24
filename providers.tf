1244df7e5c0976989c57eddffa5330b3a7f98798
dobrovolskyi <vdobrovolskyi@gmail.com>
2020-11-24 22:34:56 +0200

    cleaned creds

--- providers.tf
+++ providers.tf
@@ -8,9 +8,8 @@ provider "aws" {
 }
 
 provider "google" {
-  version     = "3.48.0"
-  credentials = var.GOOGLE_APPLICATION_CREDENTIALS
-  project     = "My First Project" # "{{YOUR GCP PROJECT}}"
-  region      = "europe-west1"
-  zone        = "europe-west1-b"
+  version = "3.48.0"
+  project = "My First Project" # "{{YOUR GCP PROJECT}}"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
 }
