6c2bb9fca0e82a22711f45aebfa6ac7a51910788
Dobrovolskyi <12254690+Dobrovolskyi@users.noreply.github.com>
2020-11-26 19:45:58 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,7 +9,6 @@ provider "aws" {
 
 provider "google" {
   version     = "3.48.0"
-  credentials = file("GOOGLE_APPLICATION_CREDENTIALS.json")
   project     = "My First Project" # "{{YOUR GCP PROJECT}}"
   region      = "europe-west1"
   zone        = "europe-west1-b"
